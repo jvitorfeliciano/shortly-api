f5b644485b590a204655ffaa97e0f60fee78eab2
João Vitor Feliciano <jvitorfeliciano1@gmail.com>
2022-12-18 19:46:36 -0300

    feat: add postUser function

--- src/controllers/authController.js
+++ src/controllers/authController.js
@@ -1,10 +1,17 @@
 import bcrypt from "bcrypt";
+import connectionDB from "../database/db.js";
 
 export const postUser = async (req, res) => {
   const { name, email, password } = res.locals.signUpInformations;
   const encryptedPassword = bcrypt.hashSync(password, 10);
-  
+
   try {
+    await connectionDB.query(
+      `INSERT INTO users (name, email, password)
+       VALUES ($1,$2,$3)`,
+      [name, email, encryptedPassword]
+    );
+    return res.sendStatus(201);
   } catch (err) {
     return res.status(500).send({ message: err.message });
   }
